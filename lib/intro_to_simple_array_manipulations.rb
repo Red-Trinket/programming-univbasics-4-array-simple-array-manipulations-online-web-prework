beba119a9ac54208bba9461832a59e9fa0cedc16
Matthew Jones <matts3371@gmail.com>
2019-09-04 22:03:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,14 @@ def using_push(array, elements)
   array.push(elements)
 end
 
-def using_unshift
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
